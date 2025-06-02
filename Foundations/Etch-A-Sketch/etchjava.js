19acc03f4b88528c44ba9aefef9ed72c6c417d71
liav lavi <liav.lavi@gmail.com>
2025-06-02 23:50:39 +0300

    Built EtchASketch basics

---
+++ Foundations/Etch-A-Sketch/etchjava.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.grid-container');
+for (let i = 0 ; i < 16 * 16 ; i++){
+const square = document.createElement('div');
+square.classList.add("grid");
+container.appendChild(square);
+}
